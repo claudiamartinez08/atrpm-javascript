01e4bb4c2a7a4e489fc7a6462b8122cb952441e9
Clau <claudia.martinez@atrapalo.com>
2024-03-14 14:00:46 +0100

    filter function

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const form = document.querySelector('#item-form');
 const input = document.querySelector('#item-input');
 const items = [];
 const list = document.querySelector('#item-list');
+const listItems = list.querySelectorAll('li');
 const clearButton = document.querySelector('#clear-button');
+const filter = document.querySelector('.filter')
+const filterInput = document.querySelector('#filter')
 
 const itemTemplate = (element) => `
     <li>
@@ -13,20 +16,44 @@ const itemTemplate = (element) => `
     </li>
 `;
 
+const checkNumberOfItems = () => {
+    if(items.length === 0) {
+        clearButton.style.display = 'none';
+        filter.style.display = 'none';
+    } else {
+        clearButton.style.display = 'block';
+        filter.style.display = 'block';
+    }
+}
+
+checkNumberOfItems()
 // Actions
 const addElement = (element) => {
     list.insertAdjacentHTML('beforeend', itemTemplate(element));
 }
+
 const removeElement = (element) => {
     element.parentElement.parentElement.remove();
     const value = element.parentElement.parentElement.innerText;
     items.pop(value)
 }
+
 const removeAllElements = () => {
     list.querySelectorAll('li').forEach((item) => {
         item.remove()
     })
     items.length = 0;
+}
+
+const filterElements = (inputValue) => {
+    const listItems = list.querySelectorAll('li');
+    listItems.forEach((i)=> {
+        if(i.innerText.toLowerCase().includes(inputValue.toLowerCase())) {
+            i.style.display = 'flex'
+        } else {
+            i.style.display = 'none'
+        }
+    })
 }
 
 // Event Listeners
@@ -35,16 +62,24 @@ form.addEventListener('submit', (e) => {
     if (input.value !== '') {
         items.push(input.value);
         addElement(input.value);
+        input.value = '';
+        checkNumberOfItems()
     }
 });
-
 
 list.addEventListener('click', (e) => {
     if (e.target.classList.contains('fa-solid')) {
         removeElement(e.target);
+        checkNumberOfItems()
     }
 });
 
 clearButton.addEventListener('click', ()=> {
     removeAllElements()
+    checkNumberOfItems()
 })
+
+filterInput.addEventListener('keyup', (e) => {
+    const inputValue = e.target.value
+    filterElements(inputValue)
+});
